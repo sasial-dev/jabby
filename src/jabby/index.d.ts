2946aa2a2d8af22514427fecd50cd9d8f045a41e
Saber <88921984+SaberClass@users.noreply.github.com>
2025-12-08 15:37:44 +0400

    Update Instance to PVInstance

--- src/jabby/index.d.ts
+++ src/jabby/index.d.ts
@@ -8,7 +8,7 @@ interface Applet<T> {
 interface WorldConfig {
     world: World,
     entities?: Map<Instance, Entity>,
-    get_entity_from_part?: ((part: BasePart) => LuaTuple<[Entity, Instance]> | undefined)
+    get_entity_from_part?: ((part: BasePart) => LuaTuple<[Entity, PVInstance]> | undefined)
 }
 
 interface SchedulerConfig {
